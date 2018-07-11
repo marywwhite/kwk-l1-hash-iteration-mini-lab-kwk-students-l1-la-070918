88fd78ed00b69706a59591b46d1c05f61ae0c217
Mary White <maryewhite2022@gmail.com>
2018-07-11 21:51:30 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -2,15 +2,18 @@
 def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
   
-   summer_olympics = {"Sydney"=>2000,
-   "Athens: "2004",
-   "Beijing: "2008,
-   "London"}
+  {Sydney: "2000",
+  Athens: "2004",
+  Beijing: "2008",
+  London: "2012"}
   
 end
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+  new_hash = create_olympics_hash
+  new_hash[:Atlanta] = "1996"
+  new_hash
 end
 
 def iterate_through_hash

68b8e5ee3e10e14aa00bc15d33d4f7958c7ea741
Mary White <maryewhite2022@gmail.com>
2018-07-11 21:43:00 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -2,6 +2,10 @@
 def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
   
+   summer_olympics = {"Sydney"=>2000,
+   "Athens": 2004,
+   "Beijing: 2008,
+   "London"}
   
 end
 
@@ -13,7 +17,7 @@ def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
   
-  summer_olympics = {"Sydney"=>2000,"Athens"=>2004,"Beijing"=>2008,"London"}
+
   
   
   
